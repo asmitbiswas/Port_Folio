dadf06c57d3a750bf02de91e3cecc968c4dc7321
asmitbiswas <asmitbiswas1308@gmail.com>
2026-07-15 22:43:41 +0530

    Added NavBar

---
+++ app/app/component/navbar.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link"
+
+export default function NavBar(){
+    return(
+        <div
+        style={{
+            // display:"flex",
+            // border:"1px solid #151515",
+            width:"200px",
+            height:"930px",
+            padding:"20px",
+            lineHeight:"2.8",
+            background:"rgb(15, 15, 15)",
+            borderRadius:"12px",
+            fontSize:"16px"
+        }}
+        >
+         <nav>
+            <ul>
+                <li><Link href="/" style={{
+                    textDecoration:"none",
+                    color:"inherit",
+                }}><h1>Home</h1></Link></li>
+            </ul>
+         </nav>
+        </div>
+    )
+}
---
+++ app/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+// import NavBar from "./component/navbar";
+
+export default function Home() {
+  return (
+    <div>
+      <h3 class="flex-1">Hello friends,</h3>
+         <img src="" alt="" />
+         <h1>Asmit Biswas</h1>
+      {/* <NavBar/> */}
+    </div>
+  );
+}
