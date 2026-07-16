5db2f10ebe754624046d138e34c6ee90d10a920a
asmitbiswas <asmitbiswas1308@gmail.com>
2026-07-16 12:45:16 +0530

    Added Contact feature

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,52 +1,53 @@
 import Image from "next/image";
 import NavBar from "./component/navbar";
+import Link from "next/link";
 
 
 export default function Home() {
   return (
     <div
-    style={{
-         fontSize:"20px",
+      style={{
+        fontSize: "20px",
         // display:"flex",
-        padding:"5px",
-        marginTop:"2rem",
-        lineHeight:"1.8"
-    }}
-    
+        padding: "5px",
+        marginTop: "2rem",
+        lineHeight: "1.8"
+      }}
+
     >
       {/* <NavBar/> */}
       <p
-      style={{
-        display:"flex",
-        // padding:"20px",
-        // width:"900px",
-        fontSize:"14px",
-        maxWidth:"1279px",
-        margin:"0 auto",
-        color:"#666",
-        marginTop:"20px"
-      }}
+        style={{
+          display: "flex",
+          // padding:"20px",
+          // width:"900px",
+          fontSize: "14px",
+          maxWidth: "900px",
+          margin: "0 auto",
+          color: "#666",
+          marginTop: "20px"
+        }}
       >Hello Friends,👋</p>
-         {/* <img src="" alt="" /> */}
-         <h1 
-         style={{
-          font:"icon",
-             maxWidth:"1279px",
-        margin:"0 auto",
-        fontSize:"40px"
-         }}
-         > I'm Asmit Biswas</h1>
-         <p
-         style={{
-          display:"flex",
+      {/* <img src="" alt="" /> */}
+      <h1
+        style={{
+          font: "icon",
+          maxWidth: "900px",
+          margin: "0 auto",
+          fontSize: "40px"
+        }}
+      > I'm Asmit Biswas</h1>
+      <p
+        style={{
+          display: "flex",
           // justifyContent:"center"
-          maxWidth:"1279px",
-          margin:"0 auto",
-          fontSize:"14px",
-          color:"#666"
-         }}
-         >Frontend-designer | School Student</p>
-         {/* <p
+          maxWidth: "900px",
+          margin: "0 auto",
+          fontSize: "14px",
+          color: "#666"
+        }}
+      >Frontend-designer | School Student</p>
+      {/* <p
             style={{
         display:"flex",
         padding:"20px",
@@ -55,28 +56,57 @@ export default function Home() {
               margin:"0 auto"
       }}
          >I'm Asmit Biswas</p> */}
-        <h2
+      <h2
         style={{
-          padding:"10px",
-          maxWidth:"1300px",
+          padding: "10px",
+          maxWidth: "900px",
+          margin: "0 auto",
+          color: "rgb(124, 124, 124)",
+          display: 'flex'
+        }}
+      >
+        About me  .
+      </h2>
+      {/* <p style={{color:"purple", display:"flex"}}>.</p> */}
+      <p
+        style={{
+          maxWidth: "900px",
+          margin: "0 auto",
+          fontSize: "16px",
+          color: "rgb(124, 124, 124)"
+        }}
+      >
+        I'm a teen school student and a Web designer. I've 3+ years of experience in web-dev. I can also do simple backend too..  I contribute to Github, built DevVault and worked in JISCE Gaming Club web 👾. Also i worked in frameworks like React, Next etc. I am intrested in Music, Gaming, Coding and Badminton sports.
+      </p>
+      <h2
+        style={{
+          padding: "10px",
+          maxWidth: "900px",
+          margin: "0 auto",
+          color: "rgb(124, 124, 124)",
+          // display: "flex"
+        }}
+      >
+        Contact.
+        With me,
+      </h2>
+        <div
+        style={{
+          display:"flex",
+          maxWidth:"900px",
           margin:"0 auto",
-          color:"rgb(124, 124, 124)",
-          display:'flex'
+         padding:"10px"
         }}
         >
-          About me  .
-        </h2>
-        {/* <p style={{color:"purple", display:"flex"}}>.</p> */}
-       <p
-       style={{
-        maxWidth:"1279px",
-        margin:"0 auto",
-        fontSize:"16px",
-        color:"rgb(124, 124, 124)"
-       }}
-       >
-        I'm a teen school student and a Web designer. I've 3+ years of experience in web-dev. I can also do simple backend too..  I contribute to Github, built DevVault and worked in JISCE Gaming Club web 👾. Also i worked in frameworks like React, Next etc. I am intrested in Music, Gaming, Coding and Badminton sports.
-       </p>
+        <img style={{ width: "50px", height:"50px" }} src="GitHub.png" alt="" />
+        
+         <a 
+         style={{
+          color:"rgb(124 124 124)",
+          wordSpacing:"5px"
+         }}
+         href="https://github.com/asmitbiswas"> : asmitbiswas</a>
+        </div>
     </div>
   );
 }
