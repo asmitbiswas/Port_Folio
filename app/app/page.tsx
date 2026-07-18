85d9c052bd797ddadfb7f13697deba89bb14ea56
asmitbiswas <asmitbiswas1308@gmail.com>
2026-07-18 23:10:12 +0530

    Fixing bug

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -8,14 +8,30 @@ export default function Home() {
     <div
       style={{
         fontSize: "20px",
-        // display:"flex",
-        padding: "5px",
+        display:"flex",
+        padding: "30px",
         // marginTop: "2rem",
         lineHeight: "1.8",
-        color:"white",
+        color:"Black",
+
       }}
 
     >
+      <div>
+        <img 
+      style={{
+        width:"100px",
+        height:"100px",
+        borderRadius:"50px",
+      }}
+      src="WhatsApp Image 2026-07-18 at 11.00.05 PM.jpeg" alt="" />
+      </div>
+      <h1
+          style={{
+        maxWidth:"1px",
+        margin:"0 auto"
+      }}
+      >AsmitCoder</h1>
       </div>
   );
 }
