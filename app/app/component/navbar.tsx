93fef257be19046c00adec3cef0f80485a2a732c
asmitbiswas <asmitbiswas1308@gmail.com>
2026-07-18 22:28:35 +0530

    Done navbar

--- app/app/component/navbar.tsx
+++ app/app/component/navbar.tsx
@@ -1,71 +1,88 @@
 import Link from "next/link"
 
-export default function NavBar(){
-    return(
-        <div
-        style={{
-            // position:"fixed",
-            display:"flex",
-            border:"1px solid rgb(211, 211, 211)",
-            // background:"#1f1e1e",
-            // maxWidth:"200rem",
-            // minHeight:"100px",
-            padding:"14px",
-            color:"black"
-        }}
+export default function NavBar() {
+    return (
+      <div
+      style={{
+        maxWidth:"300px",
+        margin:"0 auto"
+      }}
+      >
+          <div
+            style={{
+                // position:"fixed",
+                display: "flex",
+                border: "1px solid rgb(211, 211, 211)",
+                // background:"#1f1e1e",
+                maxWidth:"450px",
+                // minHeight:"100px",
+                padding: "14px",
+                color: "black",
+                borderRadius:"12px",
+                justifyContent:"center",
+                alignItems:"center"
+            }}
         >
-            <h1>Asmit_Coder</h1>
             <div
-            style={{
-                display:"flex",
-                maxWidth:"1px",
-                margin:"0 auto",
-                // lineHeight:"1.8"
-            }}
+                style={{
+                    display: "flex",
+                    maxWidth:"1px",
+                    margin:"0 auto",
+                    justifyContent:"center",
+                    alignContent:"center"
+                    // lineHeight:"1.8"
+                }}
             >
-              
-                    <img 
-            style={{
-                width:"50px",
-                height:"50px",
-                // display:"flex",
-                // justifyItems:"center",
-                maxWidth:"2000px",
-                margin:"0 auto",
-            }}
-            src="home-icon.svg" alt="" />
-              <p style={{
-                    color:"rgb(212 212 212 )"
-                }}>a</p>
-            <img
-              style={{
-                width:"50px",
-                height:"50px",
-                // display:"flex",
-                // justifyItems:"center",
-                maxWidth:"2000px",
-                margin:"0 auto"
-            }}
-            src="envelop.svg" alt="" />
-                  <p style={{
-                    color:"rgb(212 212 212 )"
-                }}>a</p>
+
+                <Link href="/Home">
+                <img
+                    style={{
+                        width: "50px",
+                        height: "50px",
+                        // display:"flex",
+                        // justifyItems:"center",
+                        maxWidth: "2000px",
+                        margin: "0 auto",
+                    }}
+                    src="home-icon.svg" alt="" />
+                    </Link>
+             
+                <img
+                    style={{
+                        width: "50px",
+                        height: "50px",
+                        // display:"flex",
+                        // justifyItems:"center",
+                        maxWidth: "2000px",
+                        margin: "0 auto"
+                    }}
+                    src="envelop.svg" alt="" />
+             
+                <img
+                    style={{
+                        width: "50px",
+                        height: "50px",
+                        // display:"flex",
+                        // justifyItems:"center",
+                        maxWidth: "2000px",
+                        margin: "0 auto"
+                    }}
+                    src="hammer.svg" alt="" />
+            
             <img 
-               style={{
-                width:"50px",
-                height:"50px",
-                // display:"flex",
-                // justifyItems:"center",
-                maxWidth:"2000px",
-                margin:"0 auto"
-            }}
-            src="hammer.svg" alt="" />
-              <p style={{
-                    color:"rgb(212 212 212 )"
-                }}>a</p>
+              style={{
+                        width: "50px",
+                        height: "50px",
+                        // display:"flex",
+                        // justifyItems:"center",
+                        maxWidth: "2000px",
+                        margin: "0 auto"
+                    }}
+            src="notebook.svg" alt="" />
 
             </div>
 
         </div>
+      </div>
     )
 }
