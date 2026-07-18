c875df17b52295159c66ebfbf013f2fc3c89e125
asmitbiswas <asmitbiswas1308@gmail.com>
2026-07-18 21:28:23 +0530

    Added navbar

--- app/app/component/navbar.tsx
+++ app/app/component/navbar.tsx
@@ -6,15 +6,55 @@ export default function NavBar(){
         style={{
             // position:"fixed",
             display:"flex",
-            border:"1px solid #000",
-            // background:"#000",
-            maxWidth:"200rem",
-            minHeight:"100px",
-            padding:"14px"
+            border:"1px solid rgb(211, 211, 211)",
+            // background:"#1f1e1e",
+            // maxWidth:"200rem",
+            // minHeight:"100px",
+            padding:"14px",
+            gap:"none"
         }}
         >
             <h1>Asmit_Coder</h1>
-            <img src="home-icon.svg" alt="" />
+            <div
+            style={{
+                display:"flex",
+                maxWidth:"2000px",
+                margin:"0 auto"
+            }}
+            >
+                    <img 
+            style={{
+                width:"50px",
+                height:"50px",
+                // display:"flex",
+                // justifyItems:"center",
+                maxWidth:"900px",
+                margin:"0 auto"
+            }}
+            src="home-icon.svg" alt="" />
+            <img
+              style={{
+                width:"50px",
+                height:"50px",
+                // display:"flex",
+                // justifyItems:"center",
+                maxWidth:"900px",
+                margin:"0 auto"
+            }}
+            src="envelop.svg" alt="" />
+
+            <img 
+               style={{
+                width:"50px",
+                height:"50px",
+                // display:"flex",
+                // justifyItems:"center",
+                maxWidth:"900px",
+                margin:"0 auto"
+            }}
+            src="hammer.svg" alt="" />
+            </div>
+
         </div>
     )
 }
